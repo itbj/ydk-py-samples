764f86c5e7cc4925c245a46d7bdff7410f321342
Santiago Alvarez <saalvare@cisco.com>
2019-02-05 15:03:22 -0800

    Add sample apps to configure XR CDP using gNMI
    Includes four boilerplate apps for performing CRUD operations on the
    configuration of the CDP protocol using gNMI. It also includes four
    custom apps to create different CDP configurations and to delete the
    entire protocol configuration:
    gn-create-xr-cdp-cfg-10-ydk.py - create boilerplate
    gn-create-xr-cdp-cfg-20-ydk.py - Basic CDP
    gn-create-xr-cdp-cfg-22-ydk.py - CDP w/custom timers
    gn-create-xr-cdp-cfg-24-ydk.py - CDP w/adjacency logging
    gn-delete-xr-cdp-cfg-10-ydk.py - delete boilerplate
    gn-delete-xr-cdp-cfg-20-ydk.py - delete all CDP configuration
    gn-read-xr-cdp-cfg-10-ydk.py   - read boilerplate
    gn-update-xr-cdp-cfg-10-ydk.py - update boilerplate

---
+++ samples/basic/crud/models/cisco-ios-xr/Cisco-IOS-XR-cdp-cfg/gn-delete-xr-cdp-cfg-20-ydk.py
@@ -0,0 +1,81 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Delete all config data for model Cisco-IOS-XR-cdp-cfg.
+
+usage: gn-delete-xr-cdp-cfg-20-ydk.py [-h] [-v] device
+
+positional arguments:
+  device         gNMI device (ssh://user:password@host:port)
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.path import Repository
+from ydk.services import CRUDService
+from ydk.gnmi.providers import gNMIServiceProvider
+from ydk.models.cisco_ios_xr import Cisco_IOS_XR_cdp_cfg \
+    as xr_cdp_cfg
+import os
+import logging
+
+
+YDK_REPO_DIR = os.path.expanduser("~/.ydk/")
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    parser.add_argument("device",
+                        help="gNMI device (ssh://user:password@host:port)")
+    args = parser.parse_args()
+    device = urlparse(args.device)
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create gNMI provider
+    repository = Repository(YDK_REPO_DIR+device.hostname)
+    provider = gNMIServiceProvider(repo=repository,
+                                   address=device.hostname,
+                                   port=device.port,
+                                   username=device.username,
+                                   password=device.password)
+    # create CRUD service
+    crud = CRUDService()
+
+    cdp = xr_cdp_cfg.Cdp()  # create object
+
+    # delete configuration on gNMI device
+    crud.delete(provider, cdp)
+
+    exit()
+# End of script
