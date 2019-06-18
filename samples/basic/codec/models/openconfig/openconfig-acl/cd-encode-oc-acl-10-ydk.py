2fd0cbf01bde30886716bc92b229cb90d18dd8dc
Dasom Eom <deom119@gmail.com>
2019-06-17 17:49:22 -0700

    Add samples apps to encode ACL config using OC
    Includes one boilerplate app and three custom apps to encode access lists configuration using the OpenConfig data model:
    cd-encode-oc-acl-10-ydk.py - encode boilerplate
    cd-encode-oc-acl-30-ydk.py - allow one host
    cd-encode-oc-acl-32-ydk.py - allow multiple hosts
    cd-encode-oc-acl-34-ydk.py - multiple hosts w/ codepoint

---
+++ samples/basic/codec/models/openconfig/openconfig-acl/cd-encode-oc-acl-10-ydk.py
@@ -0,0 +1,73 @@
+#!/usr/bin/env python
+#
+# Copyright 2016 Cisco Systems, Inc.
+#
+# Licensed under the Apache License, Version 2.0 (the "License");
+# you may not use this file except in compliance with the License.
+# You may obtain a copy of the License at
+#
+#     http://www.apache.org/licenses/LICENSE-2.0
+#
+# Unless required by applicable law or agreed to in writing, software
+# distributed under the License is distributed on an "AS IS" BASIS,
+# WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+# See the License for the specific language governing permissions and
+# limitations under the License.
+#
+
+"""
+Encode configuration for model openconfig-acl.
+
+usage: cd-encode-oc-acl-10-ydk.py [-h] [-v] device
+
+optional arguments:
+  -h, --help     show this help message and exit
+  -v, --verbose  print debugging messages
+"""
+
+from argparse import ArgumentParser
+from urlparse import urlparse
+
+from ydk.services import CodecService
+from ydk.providers import CodecServiceProvider
+from ydk.models.openconfig import openconfig_acl \
+    as oc_acl
+import logging
+
+
+def config_acl(acl):
+    """Add config data to acl object."""
+    pass
+
+
+if __name__ == "__main__":
+    """Execute main program."""
+    parser = ArgumentParser()
+    parser.add_argument("-v", "--verbose", help="print debugging messages",
+                        action="store_true")
+    args = parser.parse_args()
+
+    # log debug messages if verbose argument specified
+    if args.verbose:
+        logger = logging.getLogger("ydk")
+        logger.setLevel(logging.INFO)
+        handler = logging.StreamHandler()
+        formatter = logging.Formatter(("%(asctime)s - %(name)s - "
+                                      "%(levelname)s - %(message)s"))
+        handler.setFormatter(formatter)
+        logger.addHandler(handler)
+
+    # create codec provider
+    provider = CodecServiceProvider(type="xml")
+
+    # create codec service
+    codec = CodecService()
+
+    acl = oc_acl.Acl()  # create object
+    config_acl(acl)  # add object configuration
+
+    # encode and print object
+    # print(codec.encode(provider, acl))
+
+    exit()
+# End of script
