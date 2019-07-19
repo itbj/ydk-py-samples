81240128012bd95e00289d6f6f6242ffc4ac9340
Dasom Eom <deom119@gmail.com>
2019-07-19 10:51:00 -0700

    Fix Codec apps for XR ISIS

--- samples/basic/codec/models/cisco-ios-xr/Cisco-IOS-XR-clns-isis-cfg/cd-encode-xr-clns-isis-cfg-40-ydk.py
+++ samples/basic/codec/models/cisco-ios-xr/Cisco-IOS-XR-clns-isis-cfg/cd-encode-xr-clns-isis-cfg-40-ydk.py
@@ -57,8 +57,7 @@ def config_isis(isis):
     metric_style = af.af_data.metric_styles.MetricStyle()
     metric_style.style = xr_clns_isis_cfg.IsisMetricStyle.new_metric_style
     metric_style.level = xr_clns_isis_datatypes.IsisInternalLevel.not_set
-    transition_state = xr_clns_isis_cfg.IsisMetricStyleTransition.disabled
-    metric_style.transition_state = transition_state
+
     # segment routing
     mpls = xr_clns_isis_cfg.IsisLabelPreference.ldp
     af.af_data.segment_routing.mpls = mpls
@@ -77,7 +76,7 @@ def config_isis(isis):
     interface_af.interface_af_data.running = Empty()
     # segment routing
     prefix_sid = interface_af.interface_af_data.PrefixSid()
-    prefix_sid.type = xr_clns_isis_cfg.Isissid.absolute
+    prefix_sid.type = xr_clns_isis_cfg.Isissid1.absolute
     prefix_sid.value = 16041
     prefix_sid.php = xr_clns_isis_cfg.IsisphpFlag.enable
     explicit_null = xr_clns_isis_cfg.IsisexplicitNullFlag.disable
